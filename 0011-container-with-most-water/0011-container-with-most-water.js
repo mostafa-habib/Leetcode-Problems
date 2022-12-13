e5db867aeb4087ce1de88f386fab561f37e0efb7
Mostafa Habib <mostafahabib866@gmail.com>
2022-12-13 14:41:49 +0200

    Time: 95 ms (79.46%), Space: 49.7 MB (57.39%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = (height) => {
+	let result = 0,
+		left = 0,
+		right = height.length - 1;
+
+	while (left < right) {
+		let smallestSide = Math.min(height[left], height[right]);
+		let area = (right - left) * smallestSide;
+
+		if (area > result) result = area;
+
+		if (height[left] < height[right]) left++;
+		else right--;
+	}
+
+	return result;
+};
