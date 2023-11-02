0601f3ab4bda8627726ac59f1a0034136ce04dee
Mostafa Habib <mostafahabib866@gmail.com>
2023-11-02 16:21:45 +0200

    Time: 510 ms (11.08%), Space: 42.4 MB (94.78%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -4,30 +4,26 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-   let sum = 0;
+  let sum = 0;
   let arr = [];
-  if(nums.length === 3)
-    return nums[0] + nums[1] + nums[2];
-  
-  let closestElement = nums[0] + nums[1] + nums[2]; 
+  if (nums.length === 3) return nums[0] + nums[1] + nums[2];
+
+  let closestElement = nums[0] + nums[1] + nums[2];
   let minDifference = Math.abs(target - closestElement);
-  for(let i = 0; i <= nums.length - 3; i++) {
-    for(let x = i + 1; x < nums.length - 1; x++) {
-      for(let y = x + 1; y < nums.length; y++) {
-      // i = 0 0 0 1
-      // x = 1 1 2 2
-      // y = 2 3 3 3
-      sum = nums[i] + nums[x] + nums[y];
-    const currentDifference = Math.abs(target - sum);
-    if (currentDifference < minDifference) {
-      closestElement = sum;
-      minDifference = currentDifference;
+  for (let i = 0; i <= nums.length - 3; i++) {
+    for (let x = i + 1; x < nums.length - 1; x++) {
+      for (let y = x + 1; y < nums.length; y++) {
+        // i = 0 0 0 1
+        // x = 1 1 2 2
+        // y = 2 3 3 3
+        sum = nums[i] + nums[x] + nums[y];
+        const currentDifference = Math.abs(target - sum);
+        if (currentDifference < minDifference) {
+          closestElement = sum;
+          minDifference = currentDifference;
+        }
+      }
     }
-      
-    }
-    }
-    
-    
   }
   return closestElement;
 }
